6fa8520406c331578726624539d10f173767767c
Mcdull <1042762344@qq.com>
2020-05-20 20:33:12 +0800

    Update hmd2.sh

--- hmd2.sh
+++ hmd2.sh
@@ -1,3 +1,3 @@
 link="http://www.tvboxnow.com/thread-5335878-1-1.html"
-read EP
+read -p "第幾集？" EP
 curl $link | grep torrent | grep H265 | sed 's/.*href=\"\(at.*\)\"/\1/g' | grep EP$EP | sed 's/\".*//g'
