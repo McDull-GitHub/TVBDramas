3ce85240ce3a06da0504b8fadd577135882c8ab0
Mcdull <1042762344@qq.com>
2020-05-20 23:24:34 +0800

    Update hmd2.sh

--- hmd2.sh
+++ hmd2.sh
@@ -3,7 +3,7 @@ link="http://www.tvboxnow.com/thread-5335878-1-1.html"
 read -p "第幾集？" EP
 curl $link | grep torrent | grep H265 | sed 's/.*href=\"\(at.*\)\"/\1/g' | grep EP$EP | sed 's/\".*//g'
 link2download=$(curl $link | grep torrent | grep H265 | sed 's/.*href=\"\(at.*\)\"/\1/g' | grep EP$EP | sed 's/\".*//g')
-download_torrent=$(curl -L http://www.tvboxnow.com/$link2download | sed 's/.*\"\(at.*\)\"/\1/g' | sed 's/\".*//g')
+download_torrent=$(curl -L "http://www.tvboxnow.com/$link2download" | sed 's/.*\"\(at.*\)\"/\1/g' | sed 's/\".*//g')
 echo http://www.tvboxnow.com/$download_torrent
 echo http://www.tvboxnow.com/$download_torrent
 echo http://www.tvboxnow.com/$download_torrent
