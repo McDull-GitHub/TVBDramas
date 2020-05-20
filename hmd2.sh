ae36c515340b9b2ec9185b468c4ecc434bff365d
Mcdull <1042762344@qq.com>
2020-05-20 21:59:45 +0800

    Update hmd2.sh

--- hmd2.sh
+++ hmd2.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 link="http://www.tvboxnow.com/thread-5335878-1-1.html/"
 read -p "第幾集？" EP
+curl $link | grep torrent | grep H265 | sed 's/.*href=\"\(at.*\)\"/\1/g' | grep EP$EP | sed 's/\".*//g'
 link2download=$(curl $link | grep torrent | grep H265 | sed 's/.*href=\"\(at.*\)\"/\1/g' | grep EP$EP | sed 's/\".*//g')
 echo $link2download
 echo $link
